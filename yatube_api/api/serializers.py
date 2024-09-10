697958e8f7258a787affec54b4abe685fd0caa92
Olga Voznaya <voznaya.olga@yandex.ru>
2024-09-10 10:48:35 +0300

    Версия 3

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -19,7 +19,7 @@ class PostSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Post
-        fields = ('__all__')
+        fields = '__all__'
 
 
 class GroupSerializer(serializers.ModelSerializer):
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,44 +1,31 @@
 from django.shortcuts import get_object_or_404
 from django.utils import timezone
 from rest_framework import viewsets
-from rest_framework import permissions
 
+from api.permissions import IsAuthorOrReadOnly
 from api.serializers import CommentSerializer, GroupSerializer, PostSerializer
 from posts.models import Comment, Group, Post
-
-
-class AuthorOrReadOnly(permissions.BasePermission):
-
-    def has_permission(self, request, view):
-        return request.user.is_authenticated
-
-    def has_object_permission(self, request, view, obj):
-        return (request.method in permissions.SAFE_METHODS
-                or obj.author == request.user)
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = (AuthorOrReadOnly,)
+    permission_classes = (IsAuthorOrReadOnly,)
 
     def perform_create(self, serializer):
-        serializer.save(author=self.request.user, pub_date=timezone.now())
-
-    def perform_update(self, serializer):
-        serializer.save(author=self.request.user, pub_date=timezone.now())
+        serializer.save(author=self.request.user)
 
 
 class GroupViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
-    permission_classes = (AuthorOrReadOnly,)
+    permission_classes = (IsAuthorOrReadOnly,)
 
 
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-    permission_classes = (AuthorOrReadOnly,)
+    permission_classes = (IsAuthorOrReadOnly,)
 
     def get_post(self):
         return get_object_or_404(Post, pk=self.kwargs['post_id'])
@@ -48,7 +35,4 @@ class CommentViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(created=timezone.now(), author=self.request.user,
-                        post_id=self.get_post().pk)
-
-    def perform_update(self, serializer):
-        serializer.save(created=timezone.now(), author=self.request.user)
+                        post=self.get_post())
